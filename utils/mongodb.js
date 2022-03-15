5564534f5513b3e1d8fee6511341fe7958acdc1b
Hiep Nguyen iMac Company <nxhiep.1996@gmail.com>
2022-03-15 10:48:47 +0700

    add mongo db

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,34 +1,14 @@
 const mongoose = require('mongoose');
-const IP = process.env.IP_DB || "localhost";
-const PORT = process.env.PORT_DB || 27017;
-const DATABASE = process.env.DATABASE_NAME || "test";
-const USER = process.env.USER_DB || "";
-const PASS = process.env.PASS_DB || "";
-
 const connectDatabase = () => {
-    const DB_URL = `mongodb://${IP}:${PORT || 27017}`;
-    console.log("DB_URL ", DB_URL);
-    let mongoSetup = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-        dbName: DATABASE,
-    };
-    if (USER.length) {
-        Object.assign(mongoSetup, {
-            auth: {
-                user: USER,
-                password: PASS,
-            },
-            authSource: DATABASE,
-        });
-    }
+    const DB_URL = "mongodb+srv://hiepnx:Hiepnx27@cluster0.j5rt4.mongodb.net/data";
+    const database = "data";
     mongoose
-        .connect(DB_URL, mongoSetup)
+        .connect(DB_URL, {
+            dbName: database,
+            autoIndex: true,
+        })
         .then(() => console.log("MongoDB connected: ", DB_URL))
         .catch((err) => console.log("MongoDB initial connection error: ", err));
-
     mongoose.connection.on("error", (err) => {
         console.log("MongoDB error: ", err);
     });
