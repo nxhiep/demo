166f00573a730091fef670fe388ca65eb2d39f76
Hiep Nguyen iMac Company <nxhiep.1996@gmail.com>
2022-03-16 18:21:15 +0700

    edit export content

--- src/routes/math.router.js
+++ src/routes/math.router.js
@@ -9,6 +9,15 @@ mathRouter.post("/convert", async function (req, res) {
     if(content) {
         try {
             const dom = new JSDOM(content);
+            let elements = dom.window.document.querySelectorAll('*');
+            for(let elem of elements) {
+                elem.removeAttribute('style');
+                elem.removeAttribute('class');
+                elem.removeAttribute('onclick');
+                if(!elem.innerHTML) {
+                    elem.remove();
+                }
+            }
             let maths = dom.window.document.querySelectorAll('math');
             if(maths.length > 0) {
                 for(let math of maths) {
@@ -16,12 +25,10 @@ mathRouter.post("/convert", async function (req, res) {
                         math.outerHTML = "\\(" + Mathml2latex.convert(math.outerHTML) + "\\)";
                     }
                 }
-                res.status(200).send(dom.window.document.body.innerHTML);
-            } else {
-                res.status(200).send(content);
             }
+            res.status(200).send(dom.window.document.body.innerHTML);
         } catch(e){
-            res.status(500).send(e);
+            res.status(200).send(content);
         }
     } else {
         res.status(400).send("400 Bad request!");
